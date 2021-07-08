49f8050410bcff6562dad517bc2a0e525365bafb
Marlon Algarra <algarramarlon@hotmail.com>
2021-07-08 09:47:41 -0500

    Commit inicial

---
+++ ChartService.php
@@ -0,0 +1,13 @@
+<?php
+
+class ChartService{
+    
+    public function GetBarChartData(){
+        //Generar números aleatorios de algún tipo para gráfica tipo bar.
+    }
+
+    //Crear métodos para generar datos de gráfica tipo pie y scatter 
+
+}
+
+?>
